f6c738f770d321d39d3bfada8fbf2b2f22ff1cb2
Jordan Dalton <jordan.8474@gmail.com>
2017-05-21 21:44:09 +0100

    Update UI
    Updated the UI and changed some stuff to make the app run a bit better.

--- js/src/socket.js
+++ js/src/socket.js
@@ -23,6 +23,12 @@ function connect(){
 function onOpen(e){
     // Get blips?
     webSocket.send("getBlips");
+
+    $("#connection").removeClass("label-danger")
+        .removeClass("label-warning")
+        .addClass("label-success").text("connected");
+    $("#socket_error").text("");
+
 }
 function onMessage(e){
     var m = encodeURIComponent(e.data).match(/%[89ABab]/g);
@@ -46,9 +52,53 @@ function onMessage(e){
 }
 
 function onError(e){
-    //TODO: Handle it?
+    // from http://stackoverflow.com/a/28396165
+    var reason;
+    // See http://tools.ietf.org/html/rfc6455#section-7.4.1
+    if (event.code == 1000)
+        reason = "Normal closure, meaning that the purpose for which the connection was established has been fulfilled.";
+    else if(event.code == 1001)
+        reason = "An endpoint is \"going away\", such as a server going down or a browser having navigated away from a page.";
+    else if(event.code == 1002)
+        reason = "An endpoint is terminating the connection due to a protocol error";
+    else if(event.code == 1003)
+        reason = "An endpoint is terminating the connection because it has received a type of data it cannot accept (e.g., an endpoint that understands only text data MAY send this if it receives a binary message).";
+    else if(event.code == 1004)
+        reason = "Reserved. The specific meaning might be defined in the future.";
+    else if(event.code == 1005)
+        reason = "No status code was actually present.";
+    else if(event.code == 1006)
+       reason = "The connection was closed abnormally, e.g., without sending or receiving a Close control frame";
+    else if(event.code == 1007)
+        reason = "An endpoint is terminating the connection because it has received data within a message that was not consistent with the type of the message (e.g., non-UTF-8 [http://tools.ietf.org/html/rfc3629] data within a text message).";
+    else if(event.code == 1008)
+        reason = "An endpoint is terminating the connection because it has received a message that \"violates its policy\". This reason is given either if there is no other sutible reason, or if there is a need to hide specific details about the policy.";
+    else if(event.code == 1009)
+       reason = "An endpoint is terminating the connection because it has received a message that is too big for it to process.";
+    else if(event.code == 1010) // Note that this status code is not used by the server, because it can fail the WebSocket handshake instead.
+        reason = "An endpoint (client) is terminating the connection because it has expected the server to negotiate one or more extension, but the server didn't return them in the response message of the WebSocket handshake. <br /> Specifically, the extensions that are needed are: " + event.reason;
+    else if(event.code == 1011)
+        reason = "A server is terminating the connection because it encountered an unexpected condition that prevented it from fulfilling the request.";
+    else if(event.code == 1015)
+        reason = "The connection was closed due to a failure to perform a TLS handshake (e.g., the server certificate can't be verified).";
+    else
+        reason = "Unknown reason";
+
+    $("#socket_error").text(reason);
+
+    if (_isLive){
+        clearInterval(_invervalId);
+    }
+
 }
 function onClose(e){
+    $("#connection").removeClass("label-success")
+        .removeClass("label-warning")
+        .addClass("label-danger").text("disconnected");
+
+    if (_isLive){
+        clearInterval(_invervalId);
+    }
 
 }
 
@@ -62,6 +112,7 @@ function initBlips(blips){
         count++;
     });
     console.log(count + " blips created");
+    $("#blip_count").text(count);
 }
 
 var localCache = {};
@@ -74,7 +125,9 @@ function playerLeft(playerName){
 }
 
 function doPlayerUpdate(players){
+    var playerCount = 0;
     players.forEach(function(plr){
+        playerCount ++;
         if (plr == null) return;
 
         if (plr.name in localCache){
@@ -107,7 +160,6 @@ function doPlayerUpdate(players){
             }
 
         }else{
-
             localCache[plr.name] = {};
             localCache[plr.name].player = plr;
             var obj = new MarkerObject(plr.name, new Coordinates(plr.x, plr.y, plr.z), MarkerTypes.normal, "A player", "", "");
@@ -115,5 +167,8 @@ function doPlayerUpdate(players){
 
             localCache[plr.name].marker = _MAP_markerStore.length - 1;
         }
-        });
+    });
+
+    console.log("Playercount: " + playerCount);
+    $("#player_count").text(playerCount);
 }
