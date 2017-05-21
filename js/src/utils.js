f5bafc9ec835dfb079bdcf0246523139dc3e6954
Jordan Dalton <jordan.8474@gmail.com>
2017-05-21 19:19:34 +0100

    feat: inital commits
    Images for the atlas map
    
    Add missing atlas tiles
    
    Add more atlas tiles
    
    Add road tiles
    
    Add satalite tiles
    
    Add uv-invert tiles
    
    Add js for map related stuff
    
       Initializes the maps, controls and events.
    
    Update tile handling
    
        Map can now let user's pan anywhere, showing them the map again. Before the map would just disapear when panned too far.
    
    Add marker types
    
        Marker types been added to allow the correctt type to have the correct image from the spritesheet.
    
    Add styles
    
        Added the CSS files for styling the app
    
    Add init file
    
        The init file will handle the initialization of the map.
    
    Add objects.js
    
        This file contains the various objects that the app will use.
    
    Add utils.js
    
        The utils file mainly contains utility methods such as game coords to map coords
    
    Add app.js
    
        Contains various JQuery plugins such as modernizer
    
    Add sockets.js
    
        This file handles the websocket connection.
        It also updates the player markers and blips received from the game server.
    
    Add index.php
    
        The main page for the app
    
    Add minified markers file
    
        I think minified files are loaded quicker and the markers file is big so, it's now minified.
    
    Moved unminified files to js/src
    
        Unminified files are now in their own folder and should be used when developing.

---
+++ js/src/utils.js
@@ -0,0 +1,59 @@
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+}
+var game_1_x = 1972.606;
+var game_1_y = 3817.044;
+var map_1_lng = -60.8258056640625;
+var map_1_lat = 72.06379257078102;
+var game_2_x = -1154.11;
+var game_2_y = -2715.203;
+var map_2_lng = -72.1417236328125;
+var map_2_lat = 48.41572128171852;
+
+function convertToGame(lat, lng) {
+    var rX = game_1_x + (lng - map_1_lng) * (game_1_x - game_2_x) / (map_1_lng - map_2_lng);
+    var rY = game_1_y + (lat - map_1_lat) * (game_1_y - game_2_y) / (map_1_lat - map_2_lat);
+    return result = {
+        x: rX,
+        y: rY
+    }
+}
+
+function convertToGameCoord(lat, lng) {
+    var rX = game_1_x + (lng - map_1_lng) * (game_1_x - game_2_x) / (map_1_lng - map_2_lng);
+    var rY = game_1_y + (lat - map_1_lat) * (game_1_y - game_2_y) / (map_1_lat - map_2_lat);
+    return result = {
+        x: rX,
+        y: rY,
+        z: 0
+    }
+}
+
+function convertToMap(x, y) {
+    var rLng = map_1_lng + (x - game_1_x) * (map_1_lng - map_2_lng) / (game_1_x - game_2_x);
+    var rLat = map_1_lat + (y - game_1_y) * (map_1_lat - map_2_lat) / (game_1_y - game_2_y);
+    return result = {
+        lat: rLat,
+        lng: rLng
+    }
+}
+
+function convertToMapGMAP(x, y) {
+    var rLng = map_1_lng + (x - game_1_x) * (map_1_lng - map_2_lng) / (game_1_x - game_2_x);
+    var rLat = map_1_lat + (y - game_1_y) * (map_1_lat - map_2_lat) / (game_1_y - game_2_y);
+    return new google.maps.LatLng(rLat, rLng)
+}
+
+function convertToMapGMAPcoord(coord) {
+    var rLng = map_1_lng + (coord.x - game_1_x) * (map_1_lng - map_2_lng) / (game_1_x - game_2_x);
+    var rLat = map_1_lat + (coord.y - game_1_y) * (map_1_lat - map_2_lat) / (game_1_y - game_2_y);
+    return new google.maps.LatLng(rLat, rLng)
+}
+
+function stringCoordToFloat(coord) {
+    return result = {
+        x: parseFloat(coord.x),
+        y: parseFloat(coord.y),
+        z: parseFloat(coord.z),
+    }
+};
