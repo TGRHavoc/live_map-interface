f52c647b44408966a420647440b0b7cfaa62c253
AciD <xlxAciDxlx@users.noreply.github.com>
2017-09-22 08:54:49 -0400

    style: add .editorconfig
    Added `.editorconfig` to standardize code formatting
    Fixed formatting of neccesary files

--- utils/minifier.php
+++ utils/minifier.php
@@ -7,13 +7,13 @@
  * @return string
  */
 function concatenateFiles($files) {
-    $buffer = '';
+	$buffer = '';
 
-    foreach($files as $file) {
-        $buffer .= file_get_contents($file);
-    }
+	foreach($files as $file) {
+		$buffer .= file_get_contents($file);
+	}
 
-    return $buffer;
+	return $buffer;
 }
 
 /**
@@ -21,15 +21,15 @@ function concatenateFiles($files) {
  * @return mixed|string
  */
 function minifyCSS($files) {
-    $buffer = concatenateFiles($files);
+	$buffer = concatenateFiles($files);
 
-    $buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
-    $buffer = str_replace(["\r\n","\r","\n","\t",'  ','    ','     '], '', $buffer);
-    $buffer = preg_replace(['(( )+{)','({( )+)'], '{', $buffer);
-    $buffer = preg_replace(['(( )+})','(}( )+)','(;( )*})'], '}', $buffer);
-    $buffer = preg_replace(['(;( )+)','(( )+;)'], ';', $buffer);
+	$buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
+	$buffer = str_replace(["\r\n","\r","\n","\t",'  ','    ','     '], '', $buffer);
+	$buffer = preg_replace(['(( )+{)','({( )+)'], '{', $buffer);
+	$buffer = preg_replace(['(( )+})','(}( )+)','(;( )*})'], '}', $buffer);
+	$buffer = preg_replace(['(;( )+)','(( )+;)'], ';', $buffer);
 
-    return $buffer;
+	return $buffer;
 }
 
 /**
@@ -37,58 +37,58 @@ function minifyCSS($files) {
  * @return mixed|string
  */
 function minifyJS($files) {
-    $buffer = concatenateFiles($files);
+	$buffer = concatenateFiles($files);
 
-    $buffer = preg_replace("/((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:\/\/.*))/", "", $buffer);
-    $buffer = str_replace(["\r\n","\r","\t","\n",'  ','    ','     '], '', $buffer);
-    $buffer = preg_replace(['(( )+\))','(\)( )+)'], ')', $buffer);
+	$buffer = preg_replace("/((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:\/\/.*))/", "", $buffer);
+	$buffer = str_replace(["\r\n","\r","\t","\n",'  ','    ','     '], '', $buffer);
+	$buffer = preg_replace(['(( )+\))','(\)( )+)'], ')', $buffer);
 
-    return $buffer;
+	return $buffer;
 }
 
 function printCss($debug){
-    $cssFiles = array("style/src/bootstrap.css",
-                        "style/src/bootstrap-theme.css",
-                        "style/src/style.css");
-    if($debug){
-        foreach($cssFiles as $fname){
-            echo "<link type=\"text/css\" rel=\"stylesheet\" href=\"$fname\">";
-        }
-    }else{
-        echo "<style>";
-        echo minifyCSS($cssFiles);
-        echo "</style>";
-    }
+	$cssFiles = array("style/src/bootstrap.css",
+						"style/src/bootstrap-theme.css",
+						"style/src/style.css");
+	if($debug){
+		foreach($cssFiles as $fname){
+			echo "<link type=\"text/css\" rel=\"stylesheet\" href=\"$fname\">";
+		}
+	}else{
+		echo "<style>";
+		echo minifyCSS($cssFiles);
+		echo "</style>";
+	}
 }
 
 function printFirstJs($debug){
-    $jsFiles = array("js/src/objects.js", "js/src/utils.js",
-                    "js/src/map.js", "js/src/markers.js",
-                    "js/src/init.js", "js/src/socket.js");
+	$jsFiles = array("js/src/objects.js", "js/src/utils.js",
+					"js/src/map.js", "js/src/markers.js",
+					"js/src/init.js", "js/src/socket.js");
 
-    if($debug){
-        foreach($jsFiles as $fname){
-            echo "<script type=\"text/javascript\" src=\"$fname\"></script>\n";
-        }
-    }else{
-        echo "<script>";
-        echo minifyJs($jsFiles);
-        echo "</script>";
-    }
+	if($debug){
+		foreach($jsFiles as $fname){
+			echo "<script type=\"text/javascript\" src=\"$fname\"></script>\n";
+		}
+	}else{
+		echo "<script>";
+		echo minifyJs($jsFiles);
+		echo "</script>";
+	}
 }
 
 function printLastJs($debug){
-    $jsFiles = array("js/src/controls.js");
+	$jsFiles = array("js/src/controls.js");
 
-    if($debug){
-        foreach($jsFiles as $fname){
-            echo "<script type=\"text/javascript\" src=\"$fname\"></script>\n";
-        }
-    }else{
-        echo "<script>";
-        echo minifyJs($jsFiles);
-        echo "</script>";
-    }
+	if($debug){
+		foreach($jsFiles as $fname){
+			echo "<script type=\"text/javascript\" src=\"$fname\"></script>\n";
+		}
+	}else{
+		echo "<script>";
+		echo minifyJs($jsFiles);
+		echo "</script>";
+	}
 }
 
 
