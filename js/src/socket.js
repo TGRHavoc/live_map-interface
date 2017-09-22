fe5281ba347ace7deccc45f4c8bb0c946458393f
Jordan Dalton <jordan.8474@gmail.com>
2017-09-22 16:06:23 +0100

    fix: minifying issues
    Setting "$debug" to false  now correctly minifies the JS code. Before, it would minify it but syntax errors (missing semicolons) would cause the code to not execute.
    I've also added final_newline to the editor config (I can't remember where but, I heard it's better to have them).

--- js/src/socket.js
+++ js/src/socket.js
@@ -57,34 +57,35 @@ function onError(e){
 	// from http://stackoverflow.com/a/28396165
 	var reason;
 	// See http://tools.ietf.org/html/rfc6455#section-7.4.1
-	if (event.code == 1000)
+	if (event.code == 1000){
 		reason = "Normal closure, meaning that the purpose for which the connection was established has been fulfilled.";
-	else if(event.code == 1001)
-		reason = "An endpoint is \"going away\", such as a server going down or a browser having navigated away from a page.";
-	else if(event.code == 1002)
+	}else if(event.code == 1001){
+		reason = "Server is going down or a browser having navigated away from a page.";
+	}else if(event.code == 1002){
 		reason = "An endpoint is terminating the connection due to a protocol error";
-	else if(event.code == 1003)
-		reason = "An endpoint is terminating the connection because it has received a type of data it cannot accept (e.g., an endpoint that understands only text data MAY send this if it receives a binary message).";
-	else if(event.code == 1004)
+	}else if(event.code == 1003){
+		reason = "Wrong data type recieved by the server";
+	}else if(event.code == 1004){
 		reason = "Reserved. The specific meaning might be defined in the future.";
-	else if(event.code == 1005)
+	}else if(event.code == 1005){
 		reason = "No status code was actually present.";
-	else if(event.code == 1006)
+	}else if(event.code == 1006){
 	   reason = "The connection was closed abnormally, e.g., without sending or receiving a Close control frame";
-	else if(event.code == 1007)
-		reason = "An endpoint is terminating the connection because it has received data within a message that was not consistent with the type of the message (e.g., non-UTF-8 [http://tools.ietf.org/html/rfc3629] data within a text message).";
-	else if(event.code == 1008)
-		reason = "An endpoint is terminating the connection because it has received a message that \"violates its policy\". This reason is given either if there is no other sutible reason, or if there is a need to hide specific details about the policy.";
-	else if(event.code == 1009)
-	   reason = "An endpoint is terminating the connection because it has received a message that is too big for it to process.";
-	else if(event.code == 1010) // Note that this status code is not used by the server, because it can fail the WebSocket handshake instead.
-		reason = "An endpoint (client) is terminating the connection because it has expected the server to negotiate one or more extension, but the server didn't return them in the response message of the WebSocket handshake. <br /> Specifically, the extensions that are needed are: " + event.reason;
-	else if(event.code == 1011)
-		reason = "A server is terminating the connection because it encountered an unexpected condition that prevented it from fulfilling the request.";
-	else if(event.code == 1015)
+	}else if(event.code == 1007){
+		reason = "Server has received data within a message that was not consistent with the type of the message.";
+	}else if(event.code == 1008){
+		reason = "Server has received a message that 'violates its policy'.";
+	}else if(event.code == 1009){
+	   reason = "Server received a message that is too big for it to process.";
+	}else if(event.code == 1010){ // Note that this status code is not used by the server, because it can fail the WebSocket handshake instead.
+		reason = "Client expected the server to negotiate one or more extension, but the server didn't return them in the response message of the WebSocket handshake.\n Specifically, the extensions that are needed are: " + event.reason;
+	}else if(event.code == 1011){
+		reason = "Server encountered an unexpected condition that prevented it from fulfilling the request.";
+	}else if(event.code == 1015){
 		reason = "The connection was closed due to a failure to perform a TLS handshake (e.g., the server certificate can't be verified).";
-	else
+	}else{
 		reason = "Unknown reason (Server is probably down)";
+	}
 
 	//$("#socket_error").text(reason);
 	console.log("Socket error: " + reason);
@@ -103,19 +104,20 @@ var localCache = {};
 function playerLeft(playerName){
 	if (localCache[playerName].marker != null || localCache[playerName].marker != undefined){
 		clearMarker(localCache[playerName].marker);
-		localCache[playerName].marker = null;
+		delete localCache[playerName];
 	}
 
 	if ($("#playerSelect option[value='" + playerName + "']").length > 0){
 		$("#playerSelect option[value='" + playerName + "']").remove();
 	}
 
-	playerCount --;
+	playerCount = Object.keys(localCache).length;
+	console.log("Playerleft playercount: " + playerCount);
 	$("#player_count").text(playerCount);
 }
 
 function getPlayerInfoHtml(plr){
-	var html = '<div class="row info-body-row"><strong>Position:</strong>&nbsp;X {' + plr.pos.x.toFixed(4) + "} Y {" + plr.pos.y.toFixed(4) + "} Z {" + plr.pos.z.toFixed(4) + "}</div>"
+	var html = '<div class="row info-body-row"><strong>Position:</strong>&nbsp;X {' + plr.pos.x.toFixed(4) + "} Y {" + plr.pos.y.toFixed(4) + "} Z {" + plr.pos.z.toFixed(4) + "}</div>";
 	for(var key in plr){
 		//console.log("found key: "+ key);
 		if (key == "name" || key == "pos" || key == "icon"){ // I should probably turn this into a array or something
@@ -134,9 +136,8 @@ function getPlayerInfoHtml(plr){
 
 function doPlayerUpdate(players){
 	console.log(players);
-	var _pc = 0;
+
 	players.forEach(function(plr){
-		_pc ++;
 		if (plr == null || plr.name == undefined || plr.name == "") return;
 
 		if ( !(plr.identifer in localCache) ){
@@ -201,7 +202,7 @@ function doPlayerUpdate(players){
 
 	});
 
-	console.log("Playercount: " + _pc);
-	$("#player_count").text(_pc);
-	playerCount = _pc;
+	playerCount = Object.keys(localCache).length;
+	console.log("Playerleft playercount: " + playerCount);
+	$("#player_count").text(playerCount);
 }
--- js/src/utils.js
+++ js/src/utils.js
@@ -11,7 +11,6 @@ var map_2_lng = -72.1417236328125;
 var map_2_lat = 48.41572128171852;
 
 function convertToGame(lat, lng) {
-<<<<<<< Updated upstream
     var rX = game_1_x + (lng - map_1_lng) * (game_1_x - game_2_x) / (map_1_lng - map_2_lng);
     var rY = game_1_y + (lat - map_1_lat) * (game_1_y - game_2_y) / (map_1_lat - map_2_lat);
     return result = {
@@ -58,51 +57,3 @@ function stringCoordToFloat(coord) {
         z: parseFloat(coord.z),
     };
 };
-=======
-	var rX = game_1_x + (lng - map_1_lng) * (game_1_x - game_2_x) / (map_1_lng - map_2_lng);
-	var rY = game_1_y + (lat - map_1_lat) * (game_1_y - game_2_y) / (map_1_lat - map_2_lat);
-	return result = {
-		x: rX,
-		y: rY
-	}
-}
-
-function convertToGameCoord(lat, lng) {
-	var rX = game_1_x + (lng - map_1_lng) * (game_1_x - game_2_x) / (map_1_lng - map_2_lng);
-	var rY = game_1_y + (lat - map_1_lat) * (game_1_y - game_2_y) / (map_1_lat - map_2_lat);
-	return result = {
-		x: rX,
-		y: rY,
-		z: 0
-	}
-}
-
-function convertToMap(x, y) {
-	var rLng = map_1_lng + (x - game_1_x) * (map_1_lng - map_2_lng) / (game_1_x - game_2_x);
-	var rLat = map_1_lat + (y - game_1_y) * (map_1_lat - map_2_lat) / (game_1_y - game_2_y);
-	return result = {
-		lat: rLat,
-		lng: rLng
-	}
-}
-
-function convertToMapGMAP(x, y) {
-	var rLng = map_1_lng + (x - game_1_x) * (map_1_lng - map_2_lng) / (game_1_x - game_2_x);
-	var rLat = map_1_lat + (y - game_1_y) * (map_1_lat - map_2_lat) / (game_1_y - game_2_y);
-	return new google.maps.LatLng(rLat, rLng)
-}
-
-function convertToMapGMAPcoord(coord) {
-	var rLng = map_1_lng + (coord.x - game_1_x) * (map_1_lng - map_2_lng) / (game_1_x - game_2_x);
-	var rLat = map_1_lat + (coord.y - game_1_y) * (map_1_lat - map_2_lat) / (game_1_y - game_2_y);
-	return new google.maps.LatLng(rLat, rLng)
-}
-
-function stringCoordToFloat(coord) {
-	return result = {
-		x: parseFloat(coord.x),
-		y: parseFloat(coord.y),
-		z: parseFloat(coord.z),
-	}
-};
->>>>>>> Stashed changes
