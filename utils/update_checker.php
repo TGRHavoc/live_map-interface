b22ce22cadacd998a2e820b19a89d334bbed4fe8
Jordan Dalton <jordan.8474@gmail.com>
2017-10-20 15:57:24 +0100

    Update update_checker

--- utils/update_checker.php
+++ utils/update_checker.php
@@ -2,7 +2,7 @@
 
 class Update{
 
-    private static $version = "2.1.1";
+    private static $version = "2.1.2";
     private static $url = "https://gist.githubusercontent.com/TGRHavoc/581ec66730b7abafe49ab616db87b0bb/raw/f27c6f2573dd1df0692f97c78a73f5687d20f791/live_map.versions";
 
     public static function latestVersion(){
