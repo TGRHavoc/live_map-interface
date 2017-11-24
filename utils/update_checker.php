df73a9f453f83222fc743f7e2a1c395d26a268fe
Jordan Dalton <jordan.8474@gmail.com>
2017-11-24 21:27:07 +0000

    Update update system
    Update system now uses the new alert system. I think it looks nicer.

--- utils/update_checker.php
+++ utils/update_checker.php
@@ -4,10 +4,14 @@ class Update{
 
     private static $version = "2.1.3";
     private static $url = "https://raw.githubusercontent.com/TGRHavoc/live_map-interface/master/version.json";
+    private static $downloadUrl = "http://github.com/TGRHavoc/live_map-interface";
+    private static $latestVer = "";
 
     public static function latestVersion(){
         $data = file_get_contents(self::$url);
         $data = json_decode($data);
+
+        self::$latestVer = $data->interface;
 
         return $data->interface == self::$version;
     }
@@ -19,6 +23,24 @@ class Update{
         self::$version = $ver->interface;
     }
 
+    public static function alertJs(){
+        $arr = array(
+            sprintf("An update is available (%s -> %s)", self::$version, self::$latestVer),
+            sprintf("Please download it <a href=\'%s\'>HERE</a>", self::$downloadUrl),
+        );
+
+        // Dynamically create a array string for the above array
+        $jsArrayString = "[";
+        foreach ($arr as $key => $value) {
+            $jsArrayString .= "'%s'" . ($key == count($arr)-1 ? "" : ",");
+        }
+        $jsArrayString .= "]";
+
+        $jsArrayString = vsprintf($jsArrayString, $arr);
+
+        return "<script> createAlert('danger', 'Update available', " . $jsArrayString . "); </script>";
+
+    }
 }
 
 ?>
