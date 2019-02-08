1cf5a9d5f2f8ada854af749e1129030d0853d520
Jordan Dalton <jordan.8474@gmail.com>
2019-02-08 17:39:34 +0000

    Fixed accuracy issues in the map markers
    It was bugging me. This version still has it's issues but. it's a lot more accurate.

--- js/src/utils.js
+++ js/src/utils.js
@@ -19,39 +19,61 @@
 function isNumeric(n) {
 	return !isNaN(parseFloat(n)) && isFinite(n)
 }
-
-//These settings seem to work for tilesize 256 (the default). The resources I could find don't help here.. So, the values below are botched. And _seem_ to work alright.
-
-// Gta 5's min max bounds
-var game_min_x = -4000.00;
-var game_max_x = 6000.00;
-var game_min_y = 8000;
-var game_max_y = -4000;
-
-// Map's min max bounds based on Leaflet's shitty system
-var map_min_x = 0;
-var map_max_x = 585; // Don't fucking ask.
-var map_min_y = 17; // IDFK why this needs to be 17 but, it seems to be accurate
-var map_max_y = 256*3 - 45; // WHYYYY???
-
-function normalize(value, min, max){
+function normalize(value, min, max) {
     return Math.abs((value - min) / (max - min));
 }
 
+var game_1_x = 327.86;
+var game_1_y = 1660.16;
+
+var game_2_x = -3778.16;
+var game_2_y = -4549.6;
+
+
+var game_min_x = -4000.00;
+var game_max_x = 6000.00;
+var game_min_y = 8000.00;
+var game_max_y = -4000.00;
+var map_min_x = 0;
+var map_min_y = 0;
+
+/*
+    var rLng = map_1_lng + (x - game_1_x) * (map_1_lng - map_2_lng) / (game_1_x - game_2_x);
+    var rLat = map_1_lat + (y - game_1_y) * (map_1_lat - map_2_lat) / (game_1_y - game_2_y);
+*/
 function convertToMap(x, y) {
+    var h = _MAP_currentLayer.options.tileSize * 3,
+        w = _MAP_currentLayer.options.tileSize * 2;
+
+    var latLng1 = _MAP_map.unproject([w * 0.5, h * 0.5], _MAP_map.getMaxZoom());
+    var latLng2 = _MAP_map.unproject([0, h], _MAP_map.getMaxZoom());
+
+    var rLng = latLng1.lng + (x - game_1_x) * (latLng1.lng - latLng2.lng) / (game_1_x - game_2_x);
+    var rLat = latLng1.lat + (y - game_1_y) * (latLng1.lat - latLng2.lat) / (game_1_y - game_2_y);
+    return result = {
+        lat: rLat,
+        lng: rLng
+    };
+}
+
+function convertToMapOld(x, y) {
+    var map_max_x = _MAP_currentLayer.options.tileSize * 2,
+        map_max_y = _MAP_currentLayer.options.tileSize * 3;
+
     var xPercent = normalize(x, game_min_x, game_max_x);
     var destX = xPercent * (Math.abs(map_max_x - map_min_x)) + map_min_x;
 
     var yPercent = normalize(y, game_min_y, game_max_y);
     var destY = yPercent * (Math.abs(map_max_y - map_min_y)) + map_min_y;
-    
-    console.log(destX, destY);
-    return {x: destX, y: destY};
+
+    console.log(convertToMap(x,y));
+    console.log(_MAP_map.unproject([destX, destY], _MAP_map.getMaxZoom()));
+    return _MAP_map.unproject([destX, destY], _MAP_map.getMaxZoom());
 }
 
 function convertToMapLeaflet(x, y){
     var t = convertToMap(x, y);
-    return {lat: -t.y, lng: t.x};// Leaflet switches lat and lng around. Y gets lower the further down you go so.. Need to invert it
+    return t;
 }
 
 function stringCoordToFloat(coord) {
