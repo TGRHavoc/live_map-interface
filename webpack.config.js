ffec53668bcb4641afd813d5c557950f85b26ee3
Jordan Dalton <jordan.8474@gmail.com>
2019-02-13 14:17:24 +0000

    feat: add bundler configs
    Added the config files for webpack and crammit.

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: [
+        "./js/src/init.js",
+        "./js/src/alerter.js",
+        "./js/src/controls.js",
+        "./js/src/map.js",
+        "./js/src/markers.js",
+        "./js/src/objects.js",
+        "./js/src/socket.js",
+        "./js/src/utils.js",
+    ],
+    output: {
+        filename: "bundle.js"
+    }
+};
