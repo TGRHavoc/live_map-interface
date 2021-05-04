6dd7269ce76333b695f49f287e6e60b0f691732f
Code Server <jordan@tgrhavoc.co.uk>
2021-05-04 18:04:56 +0100

    refactor(alerts): fix alerter not minifying
    It was using an experimental JS feature.
    I think this confused gulp.

--- js/src/first_bundle/alerter.js
+++ js/src/first_bundle/alerter.js
@@ -15,52 +15,69 @@
 //      You should have received a copy of the GNU General Public License
 //      along with this program in the file "LICENSE".  If not, see <http://www.gnu.org/licenses/>.
 // ************************************************************************** //
+/**
+AlertOptions {
+    status: "warning" | "success" | "error",
+    title: string,
+    text: string,
+    effect: "fade" | "slide" | null,
+    speed: number ,
+    customClass: string | null,
+    customIcon: string | null,
+    showIcon: boolean,
+    showCloseButton: boolean,
+    autoclose: boolean,
+    autotimeout: number,
+    gap: number,
+    distance: number,
+    type: 1 | 2 | 3,
+    position: string, // e.g. "center left"
+}
+ */
 
-class Alerter {
+var Alerter = /** @class */ (function () {
+    function Alerter() {}
 
-    static DEFAULT_OPTIONS = {
-            status: 'warning', // success, error, warning
-            title: 'Warning!',
-            text: 'Notify text lorem ipsum',
-            effect: 'fade',
-            speed: 500,
-            customClass: null,
-            customIcon: null,
-            showIcon: true,
-            showCloseButton: true,
-            autoclose: true,
-            autotimeout: 20000,
-            gap: 10,
-            distance: 20,
-            type: 2,
-            position: 'right top'
-    };
     /**
      *
      * @param {Object} data Either a string (the message to be shown) or an object containing options. See https://github.com/dgknca/simple-notify#parameters
      * @returns
      */
-    static createAlert(data) {
+    Alerter.createAlert = function (data) {
         if (data == undefined || data == null) {
             console.error("Data needs to be set");
             return;
         }
-
+        var myOptionTemp;
         if (typeof (data) == "string") {
-            var str = data;
-            data = {
-                text: str
-            };
+            myOptionTemp = { text: data };
         }
+        else {
+            myOptionTemp = data;
+        }
+        var myOptions = Object.assign(this.DEFAULT_OPTIONS, myOptionTemp);
+        return new Notify(myOptions);
+    };
+    Alerter.DEFAULT_OPTIONS = {
+        status: 'warning',
+        title: 'Warning!',
+        text: 'Notify text lorem ipsum',
+        effect: 'fade',
+        speed: 500,
+        customClass: null,
+        customIcon: null,
+        showIcon: true,
+        showCloseButton: true,
+        autoclose: true,
+        autotimeout: 20000,
+        gap: 10,
+        distance: 20,
+        type: 2,
+        position: 'bottom right'
+    };
+    return Alerter;
+}());
 
-        data = Object.assign(Alerter.DEFAULT_OPTIONS, data);
-
-        //console.log(JSON.stringify(data));
-        //console._log(JSON.stringify(settings));
-
-        return new Notify(data);
-    }
-}
 /* For testing the styles
 Alerter.createAlert({title: "success test", text: "<a href='#'>Link!</a> Some more text!", type: 1, status: "success"});
 Alerter.createAlert({title: "success test", text: "<a href='#'>Link!</a> Some more text!", type: 2, status: "success"});
