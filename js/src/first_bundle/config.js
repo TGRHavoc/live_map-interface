090b8f54bd9e9b9fc10cd2629a2d457cbfc2b725
Code Server <jordan@tgrhavoc.co.uk>
2021-05-04 21:39:29 +0100

    feat(config): make static class
    Feel like it should be static

--- js/src/first_bundle/config.js
+++ js/src/first_bundle/config.js
@@ -1,38 +1,43 @@
 
 
 class Config {
-    constructor(){
-        this.defaultConfig = {
-            debug: false,
-            tileDirectory: "images/tiles",
-            iconDirectory: "images/icons",
-            showIdentifiers: false,
-            groupPlayers: true,
-            defaults: {
-                ip: "127.0.0.1",
-                socketPort: "30121"
-            },
-            servers: []
-        };
+    constructor() { }
+    static getConfig() {
+        console.log(this.staticConfig);
+        if (JSON.stringify(this.staticConfig) === "{}") {
+            this.getConfigFileFromRemote();
+            console.warn("config didn't exist... try getting it again");
+        }
 
-        this.getConfigFile();
+        return this.staticConfig;
     }
 
-    getConfigFile(){
-        Requester.sendRequestTo("config.json", this.parseConfig(this), function(request){
+    static getConfigFileFromRemote() {
+        Requester.sendRequestTo("config.json", this.parseConfig, function (request) {
             Alerter.createAlert({
                 title: "Error getting config.json. Cannot load map",
                 text: request.textStatus.statusText
             });
         });
     }
-    
-    parseConfig(_){
-        return function(request){
-            var str = JsonStrip.stripJsonOfComments(request.responseText);
-            var configParsed = JSON.parse(str);
-            _.config = Object.assign(_.defaultConfig, configParsed);
-        }
-    }
 
+    static parseConfig(request) {
+        var str = JsonStrip.stripJsonOfComments(request.responseText);
+        var configParsed = JSON.parse(str);
+        Config.staticConfig = Object.assign(Config.defaultConfig, configParsed);
+    }
 }
+
+Config.defaultConfig = {
+    debug: false,
+    tileDirectory: "images/tiles",
+    iconDirectory: "images/icons",
+    showIdentifiers: false,
+    groupPlayers: true,
+    defaults: {
+        ip: "127.0.0.1",
+        socketPort: "30121"
+    },
+    servers: []
+};
+Config.staticConfig = {};
