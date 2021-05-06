06caed424e2b5504d09079769671612d2260a958
Jordan Dalton <jordan.8474@gmail.com>
2021-05-06 14:56:07 +0100

    refactor(utils): add extra functions into utils class

--- js/src/first_bundle/utils.js
+++ js/src/first_bundle/utils.js
@@ -1,43 +1,68 @@
-// ************************************************************************** //
-//			LiveMap Interface - The web interface for the livemap
-//					Copyright (C) 2017  Jordan Dalton
-//
-//	  This program is free software: you can redistribute it and/or modify
-//	  it under the terms of the GNU General Public License as published by
-//	  the Free Software Foundation, either version 3 of the License, or
-//	  (at your option) any later version.
-//
-//	  This program is distributed in the hope that it will be useful,
-//	  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//	  GNU General Public License for more details.
-//
-//	  You should have received a copy of the GNU General Public License
-//	  along with this program in the file "LICENSE".  If not, see <http://www.gnu.org/licenses/>.
-// ************************************************************************** //
+class Utils {
+    constructor(){}
 
-function isNumeric(n) {
-	return !isNaN(parseFloat(n)) && isFinite(n)
-}
-function normalize(value, min, max) {
-    return Math.abs((value - min) / (max - min));
-}
+    static isNumeric(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n)
+    }
+    static normalize(value, min, max) {
+        return Math.abs((value - min) / (max - min));
+    }
 
+    static convertToMap(CurrentLayer, x, y) {
+        const h = CurrentLayer.options.tileSize * 3,
+            w = CurrentLayer.options.tileSize * 2;
+
+        const latLng1 = Map.unproject([0, 0], 0);
+        const latLng2 = Map.unproject([w / 2, (h - CurrentLayer.options.tileSize)], 0);
+
+        let rLng = latLng1.lng + (x - Utils.game_1_x) * (latLng1.lng - latLng2.lng) / (Utils.game_1_x - Utils.game_2_x);
+        let rLat = latLng1.lat + (y - Utils.game_1_y) * (latLng1.lat - latLng2.lat) / (Utils.game_1_y - Utils.game_2_y);
+        return result = {
+            lat: rLat,
+            lng: rLng
+        };
+    }
+
+    static getMapBounds(layer){
+        const h = layer.options.tileSize * 3,
+            w = layer.options.tileSize * 2,
+            southWest = Map.unproject([0, h], 0),
+             northEast = Map.unproject([w, 0], 0);
+
+        return new L.LatLngBounds(southWest, northEast);
+    }
+
+
+    static convertToMapLeaflet(x, y){
+        let t = convertToMap(x, y);
+        return t;
+    }
+
+    static stringCoordToFloat(coord) {
+        return result = {
+            x: parseFloat(coord.x),
+            y: parseFloat(coord.y),
+            z: parseFloat(coord.z),
+        };
+    }
+
+
+}
 
 // :thinking: This seems to improve the accuracy. I think what the problem is, if that the images I'm using doesn't correlate 1:1 to the map I'm using as a reference
 // Reference image (for those who care): https://drive.google.com/file/d/0B-zvE86DVcv2MXhVSHZnc01QWm8/view
 // I'm pretty sure that the + and -'s account for the differences. So, maybe fine-tuning them will increase accuracy of the map.
 
 // Top left corner of the GTA Map
-var game_1_x = -4000.00 - 230;
-var game_1_y = 8000.00 + 420;
+Utils.game_1_x = -4000.00 - 230;
+Utils.game_1_y = 8000.00 + 420;
 
 // Around this location: https://tgrhavoc.me/fuck_you/retard/MGoWO6nCymLmFC3M98bXbXL7C.png.
 
 // It's the middle of the map and one tile up (in leaflet). You can find it's location on leaflet by running the line below in console
-//      var m = new L.Marker(Map.unproject([1024,1024*2],0)); m.addTo(Map);
-var game_2_x = 400.00 - 30;
-var game_2_y = -300.0 - 340.00;
+//      let m = new L.Marker(Map.unproject([1024,1024*2],0)); m.addTo(Map);
+Utils.game_2_x = 400.00 - 30;
+Utils.game_2_y = -300.0 - 340.00;
 
 // Some information. I've spent too long looking at this to clearly see a patten
 // 0 0 for leaflet = -4000 8000 for GTA
@@ -52,46 +77,6 @@ var game_2_y = -300.0 - 340.00;
 // p2: -4000, 3800 (bottom left)        0,1024                   x: 4400        x: 1024 (tilesize)
 // p3:  400, 8000 (top right)           1024,0
 // p4:  400, 3800 (bottom rigt)         1024,1024
-
-function convertToMap(x, y) {
-    var h = CurrentLayer.options.tileSize * 3,
-        w = CurrentLayer.options.tileSize * 2;
-
-    var latLng1 = Map.unproject([0, 0], 0);
-    var latLng2 = Map.unproject([w / 2, (h - CurrentLayer.options.tileSize)], 0);
-
-    var rLng = latLng1.lng + (x - game_1_x) * (latLng1.lng - latLng2.lng) / (game_1_x - game_2_x);
-    var rLat = latLng1.lat + (y - game_1_y) * (latLng1.lat - latLng2.lat) / (game_1_y - game_2_y);
-    return result = {
-        lat: rLat,
-        lng: rLng
-    };
-}
-
-function getMapBounds(layer){
-    var h = layer.options.tileSize * 3,
-        w = layer.options.tileSize * 2;
-
-    var southWest = Map.unproject([0, h], 0);
-    var northEast = Map.unproject([w, 0], 0);
-
-    return new L.LatLngBounds(southWest, northEast);
-}
-
-
-function convertToMapLeaflet(x, y){
-    var t = convertToMap(x, y);
-    return t;
-}
-
-function stringCoordToFloat(coord) {
-    return result = {
-        x: parseFloat(coord.x),
-        y: parseFloat(coord.y),
-        z: parseFloat(coord.z),
-    };
-};
-
 
 
 class JsonStrip {
@@ -165,7 +150,7 @@ class JsonStrip {
 
 class Requester {
     static sendRequestTo(url, success, error){
-        var request = new XMLHttpRequest();
+        let request = new XMLHttpRequest();
         request.open("GET", url, true);
 
         request.onload = function() {
