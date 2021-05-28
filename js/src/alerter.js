3905195e26cd14f8ec1e1527a23094e87ed296f9
Code Server <jordan@tgrhavoc.co.uk>
2021-05-28 22:31:05 +0100

    docs(alerter): add comments to alerter class

--- js/src/alerter.js
+++ js/src/alerter.js
@@ -1,30 +1,38 @@
 /**
-AlertOptions {
-    status: "warning" | "success" | "error",
-    title: string,
-    text: string,
-    effect: "fade" | "slide" | null,
-    speed: number ,
-    customClass: string | null,
-    customIcon: string | null,
-    showIcon: boolean,
-    showCloseButton: boolean,
-    autoclose: boolean,
-    autotimeout: number,
-    gap: number,
-    distance: number,
-    type: 1 | 2 | 3,
-    position: string, // e.g. "center left"
-}
+ * Option object for creating alerts
+ * @typedef {Object} AlerterOptions
+ * @property {string} status=warning            - The status of the alert. One of "warning", "success", "error"
+ * @property {string} title=Warning!            - The title of the alert
+ * @property {string} text                      - The text of the alert
+ * @property {string | null} effect=null        - The effect for the alert. Can be "fade" or "slide"
+ * @property {number} speed=500                 - The speed of the effect
+ * @property {string | null} customClass=null   - A custom class to add to the alert.
+ * @property {string | null} customIcon=null    - HTML For the icon that shows with the alert. Mostly, this is a SVG icon
+ * @property {boolean} showIcon=true            - Should the alert show the icon
+ * @property {boolean} showCloseButton=true     - Should the alert show the close button
+ * @property {boolean} autoclose=true           - Should the alert close itself after an amount of time?
+ * @property {number} autotimeout=20000         - How long after the alert has been shown should it automatically close itself (only if autoclose is true) 
+ * @property {number} gap=10                    - The margin between notifications
+ * @property {number} distance=20               - The distance to edges
+ * @property {number} type=2                    - The type of the alert
+ * @property {string} position=bottom-right     - The position of the alert. Combine x and y position. 'left', 'right', 'top', 'bottom', 'x-center', 'y-center' or use only 'center' to center both x and y.
  */
 
-export class Alerter {
+/**
+ * The main class for Alerts.
+ */
+class Alerter {
 
+    /**
+     * @param {AlerterOptions | string} data The options to use to create Alerts or the text of an alert.
+     * @constructor
+     */
     constructor(data){
+
         this.DEFAULT_OPTIONS = {
-            status: 'warning',
+            status: "warning",
             title: 'Warning!',
-            text: 'Notify text lorem ipsum',
+            text: 'This is a warning',
             effect: 'fade',
             speed: 500,
             customClass: null,
@@ -51,9 +59,9 @@ export class Alerter {
             myOptionTemp = data;
         }
 
-        var myOptions = Object.assign(this.DEFAULT_OPTIONS, myOptionTemp);
-
-        this.notify = new Notify(myOptions);
+        this.options = Object.assign(this.DEFAULT_OPTIONS, myOptionTemp);
+        
+        this.notify = new Notify(this.options);
     }
 }
 
@@ -68,3 +76,5 @@ new Alerter({title: "error test", text: "<a href='#'>Link!</a> Some more text!",
 new Alerter({title: "error test", text: "<a href='#'>Link!</a> Some more text!", type: 2, status: "error"});
 new Alerter({title: "error test", text: "<a href='#'>Link!</a> Some more text!", type: 3, status: "error"});
 */
+
+export {Alerter};
